52dd06c0e99060dd0c586b39bb5bbc28d6e93d95
micheal <mingji1116@live.com>
2021-05-31 09:25:12 +0800

    post project

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,20 @@
+const contracts = {
+  lp: {
+    3: '0x08233aaba8b922a61708d7c6423db2cd5660d412',
+    1: '',
+  },
+  mrch: {
+    3: '0x3f80b6ae16e337e72e8f2777af0f6a41de2ce8ee',
+    1: '',
+  },
+  merchNStaking: {
+    3: '0xED6B2d143e9dFc406f89868387aE5b1572742873',
+    1: '',
+  },
+  nft: {
+    3: '0x1f48B89322Fa65D10948360ab807241616Ab42C1',
+    1: '',
+  },
+}
+
+export default contracts
---
+++ src/config/constants/pools.ts
@@ -0,0 +1,22 @@
+import { PoolConfig } from './types'
+
+const pools: PoolConfig[] = [
+  {
+    id: 4,
+    apy: 120 ,
+    price: 100,
+    duration: 30,
+    startTime: 1615799486,
+    endTime: 1618462452
+  },
+  {
+    id: 5,
+    apy: 110 ,
+    price: 120,
+    duration: 60,
+    startTime: 1615799486,
+    endTime: 1621054452
+  }
+]
+
+export default pools
